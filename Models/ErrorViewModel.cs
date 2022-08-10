9f81e1509f363ca360bc389dd593b6841afd4276
Joseph Broome <josephlloydbroome@gmail.com>
2022-08-10 07:43:35 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVD_DEMO.net6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
